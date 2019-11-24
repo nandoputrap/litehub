565536f9cd8af85e8f10e570e4c8668b088f7b60
Bryanza <bryanza@Bryanzas-MacBook-Air.local>
2019-11-24 14:54:01 +0700

    fixing typo on duplicate function database

--- status-pengajuan.php
+++ status-pengajuan.php
@@ -34,24 +34,6 @@ session_start();
 	}
 ?>
 
-<?php
-function connectDB() {
-  $servername = "sql12.freesqldatabase.com";
-  $username = "sql12310568";
-  $password = "wmiLAF7a6g";
-  $dbname = "sql12310568";
-
-  // Create connection
-  $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-  // Check connection
-  if (!$conn) {
-    die("Connection failed: " + mysqli_connect_error());
-  }
-  return $conn;
-}
-?>
-
 <div class="status-pengajuan-detail section-margin">
   <div class="container">
 
