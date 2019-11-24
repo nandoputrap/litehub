632da2da50619353cc842cad88c7e7acd8a875d5
Nando P. Pratama <nandoputrap@gmail.com>
2019-11-24 14:32:44 +0700

    fadil ganteng

--- status-pengajuan-detail-editor.php
+++ status-pengajuan-detail-editor.php
@@ -1,5 +1,21 @@
 <?php
   require_once("templates/header.php");
+
+  function connectDB() {
+		$servername = "sql12.freesqldatabase.com";
+		$username = "sql12310568";
+		$password = "wmiLAF7a6g";
+		$dbname = "sql12310568";
+
+		// Create connection
+		$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+		// Check connection
+		if (!$conn) {
+			die("Connection failed: " + mysqli_connect_error());
+		}
+		return $conn;
+	}
 ?>
 
 <div class="status-pengajuan-detail section-margin">
