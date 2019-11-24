02b04fc52d0abfc04cd3ef7a5dbb01be64d5fcfe
Bryanza <bryanzanr@gmail.com>
2019-11-24 15:02:21 +0700

    Update daftar-pengajuan.php

--- daftar-pengajuan.php
+++ daftar-pengajuan.php
@@ -18,24 +18,6 @@ function connectDB() {
 }
 ?>
 
-<?php
-function connectDB() {
-  $servername = "sql12.freesqldatabase.com";
-  $username = "sql12310568";
-  $password = "wmiLAF7a6g";
-  $dbname = "sql12310568";
-
-  // Create connection
-  $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-  // Check connection
-  if (!$conn) {
-    die("Connection failed: " + mysqli_connect_error());
-  }
-  return $conn;
-}
-?>
-
 <div class="daftar-pengajuan section-margin">
   <div class="container">
 
