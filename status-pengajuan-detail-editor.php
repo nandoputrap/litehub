a8a73c176c11489d42c705fe519eaaa7d8e04a8b
Nando P. Pratama <nandoputrap@gmail.com>
2019-11-25 20:41:38 +0700

    fitur form publikasi berdasarkan status done

--- status-pengajuan-detail-editor.php
+++ status-pengajuan-detail-editor.php
@@ -104,10 +104,16 @@
                 <td><strong>Status Pengajuan</strong></td>
                 <td>
 
-                    <select class="form-control" id="kategori" style="height: 100%;">
+                    <!-- <select class="form-control" id="kategori" style="height: 100%;">
                       <option>Dalam Proses Penyuntingan</option>
                       <option>Sudah Diterbitkan</option>
-                    </select>
+                    </select> -->
+                    <div class="button dropdown">
+                      <select id="status-pengajuan" class="form-control" id="kategori" style="height: 100%;">
+                       <option>Dalam Proses Penyuntingan</option>
+                       <option value="form-publish">Sudah Diterbitkan</option>
+                      </select>
+                    </div>
 
                 </td>
               </tr>
@@ -123,82 +129,95 @@
        -->
 
         <!-- Begin Form buku untuk diterbitkan -->
-        <div class="text-center">
-          <h2>Masukkan info buku untuk diterbitkan</h2>
+        <div class="output">
+          <div class="form-publish">
+            <div class="text-center">
+              <h2>Masukkan info buku untuk diterbitkan</h2>
+            </div>
+
+            <form action="services/upload.php" method="post" enctype="multipart/form-data">
+              <input type="text" class="form-control form-register" id="insert-judulBuku" name="judulBuku" placeholder="Judul buku...">
+              <input type="text" class="form-control form-register" id="insert-namaPenulis" name="namaPenulis" placeholder="Nama penulis...">
+              <input type="text" class="form-control form-register" id="insert-namaPenerbit" name="namaPenerbit" placeholder="Nama penerbit...">
+              <input type="text" class="form-control form-register" id="insert-harga" name="Harga" placeholder="Harga...">
+              <div class="form-group">
+                <label for="kategori"></label>
+                <select class="form-control form-register form-group-kategori" name="kategori" id="kategori">
+                        <option>-Pilih Kategori-</option>
+                        <option>Umum</option>
+    										<option>Filsafat</option>
+    										<option>Psikologi</option>
+    										<option>Agama</option>
+    										<option>Sejarah</option>
+    										<option>Sosial</option>
+    										<option>Bahasa</option>
+    										<option>Sains</option>
+    										<option>Geografi</option>
+    										<option>Teknologi</option>
+    										<option>Seni</option>
+    										<option>Literatur</option>
+    										<option>Sastra</option>
+    										<option>Biografi</option>
+    										<option>Matematika</option>
+    										<option>Novel</option>
+    										<option>Cerpen</option>
+    										<option>Puisi</option>
+    										<option>Drama</option>
+    										<option>Komik</option>
+    										<option>Dongeng</option>
+    										<option>Fabel</option>
+    										<option>Mitos</option>
+                </select>
+              </div>
+
+              <div class="form-group">
+                <label for="subkategori"></label>
+                <select class="form-control form-register form-group-kategori" id="subkategori">
+                  <!-- <option>-Pilih kategori-</option> -->
+                  <option>-Pilih Sub Kategori-</option>
+                  <option>SubKategori 1</option>
+                  <option>SubKategori 2</option>
+                  <option>SubKategori 3</option>
+                  <option>SubKategori 4</option>
+                  <option>SubKategori 5</option>
+                </select>
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control form-register" placeholder="ISBN...">
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control form-register" placeholder="SKU...">
+              </div>
+              <div class="form-group">
+               <textarea class="form-control" rows="5" id="comment" name="deskripsiBuku" placeholder="Deskripsi/Sinopsis buku..."></textarea>
+              </div>
+
+              <div class="form-group">
+                <label for="exampleFormControlFile1">
+                  Pilih Cover Buku
+                </label>
+                <input type="file" class="form-control-file" name="fileToUpload" id="exampleFormControlFile1">
+              </div>
+
+              <div class="form-group">
+                <label for="exampleFormControlFile1">
+                  Format buku dalam bentuk .doc atau .docx. Format penulisan dan layout dapat melihat pada halaman <a href="#">ini.</a> Ukuran file maksimal 50 MB.
+                </label>
+                <input type="file" class="form-control-file" name="fileToUpload" id="exampleFormControlFile1">
+              </div>
+
+              <input type="hidden" id="insert-command" name="command" value="insert">
+              <button type="button" class="btn btn-primary btn-block btn-ebookhub btn-register">Simpan</button>
+            </form>
+
+
+
+          </div>
         </div>
 
-        <form action="services/upload.php" method="post" enctype="multipart/form-data">
-          <input type="text" class="form-control form-register" id="insert-judulBuku" name="judulBuku" placeholder="Judul buku...">
-          <input type="text" class="form-control form-register" id="insert-namaPenulis" name="namaPenulis" placeholder="Nama penulis...">
-          <div class="form-group">
-            <label for="kategori"></label>
-            <select class="form-control form-register form-group-kategori" name="kategori" id="kategori">
-            <option>Umum</option>
-										<option>Filsafat</option>
-										<option>Psikologi</option>
-										<option>Agama</option>
-										<option>Sejarah</option>
-										<option>Sosial</option>
-										<option>Bahasa</option>
-										<option>Sains</option>
-										<option>Geografi</option>
-										<option>Teknologi</option>
-										<option>Seni</option>
-										<option>Literatur</option>
-										<option>Sastra</option>
-										<option>Biografi</option>
-										<option>Matematika</option>
-										<option>Novel</option>
-										<option>Cerpen</option>
-										<option>Puisi</option>
-										<option>Drama</option>
-										<option>Komik</option>
-										<option>Dongeng</option>
-										<option>Fabel</option>
-										<option>Mitos</option>
-            </select>
-          </div>
-
-          <div class="form-group">
-            <label for="subkategori"></label>
-            <select class="form-control form-register form-group-kategori" id="subkategori">
-              <!-- <option>-Pilih kategori-</option> -->
-              <option>SubKategori 1</option>
-              <option>SubKategori 2</option>
-              <option>SubKategori 3</option>
-              <option>SubKategori 4</option>
-              <option>SubKategori 5</option>
-            </select>
-          </div>
-          <div class="form-group">
-            <input type="text" class="form-control form-register" placeholder="ISBN...">
-          </div>
-          <div class="form-group">
-            <input type="text" class="form-control form-register" placeholder="SKU...">
-          </div>
-          <div class="form-group">
-           <textarea class="form-control" rows="5" id="comment" name="deskripsiBuku" placeholder="Deskripsi/Sinopsis buku..."></textarea>
-          </div>
-
-          <div class="form-group">
-            <label for="exampleFormControlFile1">
-              Pilih Cover Buku
-            </label>
-            <input type="file" class="form-control-file" name="fileToUpload" id="exampleFormControlFile1">
-          </div>
-
-          <div class="form-group">
-            <label for="exampleFormControlFile1">
-              Format buku dalam bentuk .doc atau .docx. Format penulisan dan layout dapat melihat pada halaman <a href="#">ini.</a> Ukuran file maksimal 50 MB.
-            </label>
-            <input type="file" class="form-control-file" name="fileToUpload" id="exampleFormControlFile1">
-          </div>
-
-          <input type="hidden" id="insert-command" name="command" value="insert">
-        </form>
         <!-- End Form buku untuk diterbitkan -->
 
-        <button type="button" class="btn btn-primary btn-block btn-ebookhub btn-register">Simpan</button>
+
       </div>
 
 
